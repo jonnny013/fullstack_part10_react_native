79162eb2df241817735f4717abf274b1aac31fc2
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-04 13:36:56 +0800

    Move fetching into hooks directory

--- src/components/repoCards/RepositoryList.jsx
+++ src/components/repoCards/RepositoryList.jsx
@@ -1,6 +1,6 @@
 import {FlatList, View, StyleSheet} from 'react-native';
 import RepositoryItem from './RepositoryItem';
-import {useState, useEffect} from 'react';
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -8,24 +8,12 @@ const styles = StyleSheet.create({
   },
 });
 
-
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-const [repositories, setRepositories] = useState();
+  const {repositories} = useRepositories();
 
-const fetchRepositories = async () => {
-  const response = await fetch('http:/192.168.0.104:5000/api/repositories');
-  const json = await response.json();
-  console.log(json);
-  setRepositories(json);
-};
-
-useEffect(() => {
-  fetchRepositories();
-}, []);
-const repositoryNodes = repositories ? repositories.edges.map(edge => edge.node) : [];
-
+  const repositoryNodes = repositories ? repositories.edges.map(edge => edge.node) : [];
 
   return (
     <FlatList
