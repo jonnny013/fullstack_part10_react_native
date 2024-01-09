fa4c9a2b79c5eb9bc8dddc4319eb608f7882827c
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-01-09 09:43:44 +0800

    Add create user mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -15,3 +15,11 @@ export const CREATE_REVIEW = gql`
     }
   }
 `;
+
+export const SIGN_UP = gql`
+  mutation CreateUser($user: CreateUserInput!) {
+    createUser(user: $user) {
+      username
+    }
+  }
+`;
